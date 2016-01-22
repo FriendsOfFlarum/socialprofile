045ef657b47012220815185198d13460874b5734
Connor Davis <connor0095@gmail.com>
2016-01-22 22:20:14 +0000

    Finish GET API

--- src/Api/Controllers/GetSocialButtonsController.php
+++ src/Api/Controllers/GetSocialButtonsController.php
@@ -7,32 +7,18 @@ use Tobscure\JsonApi\Document;
 
 class GetSocialButtonsController extends AbstractResourceController
 {
-    /**
-     * {@inheritdoc}
-     */
-    public $serializer = 'davis\socialprofile\Api\Serializer\GetSocialButtonsSerializer';
-    public $include = ['groups'];
-    protected $users;
+    public $serializer = 'davis\socialprofile\Api\Serializers\GetSocialButtonsSerializer';
     
-    /**
-     * @param UserRepository $users
-     */
     public function __construct(UserSocialRepository $users)
     {
         $this->users = $users;
     }
-    /**
-     * {@inheritdoc}
-     */
+    
     protected function data(ServerRequestInterface $request, Document $document)
     {
         $id = array_get($request->getQueryParams(), 'user');
 
         $actor = $request->getAttribute('actor');
-        
-        if ($actor->id == $id) {
-            $this->serializer = 'davis\socialprofile\Api\Serializer\GetSocialButtonsSerializer';
-        }
 
         return $this->users->findOrFail($id, $actor);
     }
--- src/Api/Serializers/GetSocialButtonsSerializer.php
+++ src/Api/Serializers/GetSocialButtonsSerializer.php
@@ -1,33 +1,20 @@
-<?php namespace davis\socialprofile\Api\Serializer;
+<?php namespace davis\socialprofile\Api\Serializers;
 
 use Flarum\Core\Access\Gate;
 use Flarum\Api\Serializer\UserBasicSerializer;
 
 class GetSocialButtonsSerializer extends UserBasicSerializer
 {
-    /**
-     * @var Gate
-     */
     protected $gate;
 
-    /**
-     * @param Gate $gate
-     */
     public function __construct(Gate $gate)
     {
         $this->gate = $gate;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     protected function getDefaultAttributes($user)
     {
-        //$attributes = parent::getDefaultAttributes($user);
-
         $gate = $this->gate->forUser($this->actor);
-
-        //$canEdit = $gate->allows('edit', $user);
 
         $attributes = [
             'buttons'              => $user->buttons,
